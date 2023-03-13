759e6971874012f76fcd0af427f331f25e70c37c
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-13 17:36:08 +0100

    start element for ex-2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,6 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
-// Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 const listEmail = [
     'vitodepalma16@gmail.com',
@@ -46,3 +43,15 @@ function checkemail(e){
 btnCanc.addEventListener('click', function() {
     document.getElementById('resultemail').innerHTML = '';
 })
+
+// ESERCIO DADI
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const btnThrow = document.getElementById('btnThrow');
+btnThrow.addEventListener('click', throw );
+
+function throw(){
+    
+}
